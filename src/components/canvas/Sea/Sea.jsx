765e6f81c35ddcd5117d8c2f25bb8af708ee4958
Max Flowers <81179442+MaxFlowers333@users.noreply.github.com>
2022-01-31 22:50:26 -0600

    ocean

--- src/components/canvas/Sea/Sea.jsx
+++ src/components/canvas/Sea/Sea.jsx
@@ -2,6 +2,7 @@ import useStore from '@/helpers/store'
 import { Plane, shaderMaterial } from '@react-three/drei'
 import { useFrame, extend } from '@react-three/fiber'
 import { useRef, useState, Suspense } from 'react'
+import { folder, useControls } from 'leva'
 import Merkaba from '../merkaba'
 import guid from 'short-uuid'
 import { Color } from 'three'
@@ -9,13 +10,33 @@ import { Color } from 'three'
 import vertex from './glsl/shader.vert'
 import fragment from './glsl/shader.frag'
 
+
+//TOTO get shader material working
 const SeaComponent = ({ route }) => {
   const router = useStore((s) => s.router)
-  // const gltf = useLoader(GLTFLoader, 'merkabah/scene.gltf')
-  // This reference will give us direct access to the THREE.Mesh object
   const box = useRef(null)
   const ocean = useRef(null)
+  const {
+    animate,
+    bigWavesElevation,
+    bigWavesFrequency,
+    bigWaveSpeed,
+    surfaceColor,
+    depthColor,
+    colorOffset,
+    colorMultiplier,
+    smallWavesElevation,
+    smallWavesFrequency,
+    smallWavesSpeed,
+    smallIterations,
+  } = useControls({
+    animate: true,
+    colors: folder({ surfaceColor: '#c1e4fe', depthColor: '#0066b3', colorOffset: 0.08, colorMultiplier: 1.4 }),
+    bigWaves: folder({ bigWavesElevation: 0.8, bigWavesFrequency: [0.2, 0.7,], bigWaveSpeed: 0.75 }),
+    smallWaves: folder({ smallWavesElevation: 0.15, smallWavesFrequency: 3, smallWavesSpeed: 0.2, smallIterations: 4 }),
+  })
 
+  const shaderRef = useRef()
 
   // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false)
@@ -27,6 +48,7 @@ const SeaComponent = ({ route }) => {
     // ocean.current
     //   ? (ocean.current.rotation.z += hovered ? -0.01 : 0)
     //   : null
+    animate && (shaderRef.current.uTime += delta)
 
   }
   )
@@ -38,14 +60,28 @@ const SeaComponent = ({ route }) => {
         rotation-x={-Math.PI / 2}
         position={[0, 0.5, 0]}
         ref={ocean}>
-        <meshPhysicalMaterial color='purple' wireframe />
+        <seaMaterial
+          key={SeaMaterial.key}
+          ref={shaderRef}
+          uBigWavesElevation={bigWavesElevation}
+          uBigWavesFrequency={bigWavesFrequency}
+          uBigWavesSpeed={bigWaveSpeed}
+          uSurfaceColor={surfaceColor}
+          uDepthColor={depthColor}
+          uColorOffset={colorOffset}
+          uColorMultiplier={colorMultiplier}
+          uSmallWavesElevation={smallWavesElevation}
+          uSmallWavesFrequency={smallWavesFrequency}
+          uSmallWavesSpeed={smallWavesSpeed}
+          uSmallIterations={smallIterations}
+        />
       </Plane>
+
       <Suspense fallback={null}>
         <Merkaba
           scale={.02}
           rotation={[Math.PI / 4, 0, 0]}
         >
-
         </Merkaba>
       </Suspense>
 
@@ -84,8 +120,10 @@ const SeaMaterial = new shaderMaterial(
   fragment
 )
 
-// SeaMaterial.key = Math.random();
+// This is the 🔑 that HMR will renew if this file is edited
+// It works for THREE.ShaderMaterial as well as for drei/shaderMaterial
+SeaMaterial.key = guid.generate();
 
-// extend({ SeaMaterial })
+extend({ SeaMaterial })
 
 export default SeaComponent
--- src/components/canvas/Sea/glsl/shader.frag
+++ src/components/canvas/Sea/glsl/shader.frag
@@ -1,28 +1,11 @@
-#pragma glslify: cnoise2 = require(glsl-noise/classic/2d.glsl)
-#pragma glslify: cnoise3 = require(glsl-noise/classic/3d.glsl)
-uniform float uTime;
-uniform float uBigWavesElevation;
-uniform float uBigWavesSpeed;
-uniform vec2 uBigWavesFrequency;
-
-uniform float uSmallWavesElevation;
-uniform float uSmallWavesFrequency;
-uniform float uSmallWavesSpeed;
-uniform float uSmallIterations;
-
+uniform vec3 uSurfaceColor;
+uniform vec3 uDepthColor;
+uniform float uColorOffset;
+uniform float uColorMultiplier;
 varying float vElevation;
 
 void main() {
-  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-  float elevation = sin(modelPosition.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) *
-    sin(modelPosition.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) *
-    (cnoise3(vec3(modelPosition.xz / 5.0, (uTime / 5.0))) * 2.0) * uBigWavesElevation;
-  for (float i = 1.0; i <= uSmallIterations; i++) {
-    elevation -= abs(cnoise3(vec3(modelPosition.xz * uSmallWavesFrequency * i, uTime * uSmallWavesSpeed)) * uSmallWavesElevation / i);
-  }
-  modelPosition.y += elevation;
-  vec4 viewPosition = viewMatrix * modelPosition;
-  vec4 projectedPosition = projectionMatrix * viewPosition;
-  gl_Position = projectedPosition;
-  vElevation = elevation;
+  float mixStrength = (vElevation + uColorOffset) * uColorMultiplier;
+  vec3 color = mix(uDepthColor, uSurfaceColor, mixStrength);
+  gl_FragColor = vec4(color, 1.0);
 }
